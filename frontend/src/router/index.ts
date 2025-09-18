b225f3b974f0304f93dd2547ede5a94477a6be17
oleveloper <oleveloper@gmail.com>
2025-09-18 14:38:57 +0900

    fix: prevent login screen from appearing as the first page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,17 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/demo/HomeView.vue'
-import Login from '../views/common/Login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-      {
+    {
       path: '/',
-      name: 'login',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: Login,
+      name: 'home',
+      component: HomeView,
     },
     {
       path: '/quiz',
