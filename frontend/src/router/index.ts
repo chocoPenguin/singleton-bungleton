ef46f5fc48954d3eb4a3c1f0aca85b3458b62068
oleveloper <oleveloper@gmail.com>
2025-09-18 14:26:01 +0900

    feat: implement login functionality

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/demo/HomeView.vue'
+import Login from '../views/common/Login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,7 +11,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/common/LoginView.vue'),
+      component: Login,
     },
     {
       path: '/quiz',
